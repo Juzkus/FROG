2e3d45efc59fa91933e4119836274374cc7e0c6a
Paul Anderson <paulr.anderson212@gmail.com>
2019-06-24 19:45:45 -0500

    Adding simple PropertyParser implementation and test project.

---
+++ FROG.DotNet/FROG.DotNet.Tests/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using FROG.DotNet;
+
+namespace FROG.DotNet.Tests
+{
+    // TODO: change to Unit Test project?
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Running Tests.");
+
+            TestPropertyParser();
+        }
+
+        static bool TestPropertyParser()
+        {
+            var props = PropertyParser.GetPropertiesFromFile("./Resources/test.props", '=');
+
+            string name, greeting;
+            props.TryGetValue("name", out name);
+            props.TryGetValue("greeting", out greeting);
+
+            Console.WriteLine(greeting + ", " + name);
+
+            return true;
+        }
+    }
+}
--- FROG.DotNet/FROG.DotNet/PropertyParser.cs
+++ FROG.DotNet/FROG.DotNet/PropertyParser.cs
@@ -1,9 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace FROG.DotNet
 {
-    // TODO
-    public class PropertyParser
+    public static class PropertyParser
     {
+        public static IDictionary<string,string> GetPropertiesFromFile(string fileName, char delimeter)
+        {
+            IDictionary<string, string> props = new Dictionary<string, string>();
+
+            using (var reader = new StreamReader(new BufferedStream(File.OpenRead(fileName))))
+            {
+                while(!reader.EndOfStream)
+                {
+                    var kvp = reader.ReadLine().Split(delimeter);
+                    props.Add(kvp[0], kvp[1]);
+                }
+            }
+
+            return props;
+        }
     }
 }
