e1187832a7d03e13a64cb989b47c30cc992a487c
Paul Anderson <paulr.anderson212@gmail.com>
2019-07-22 21:07:23 -0500

    Starting some ideas for project structure.

---
+++ FROG.PHP.Web/index.php
@@ -0,0 +1,2 @@
+<h2>Partials (Shells)</h2>
+<a href="shells/user_login">User Login</a>
---
+++ FROG.PHP.Web/shells/user_login.php
@@ -0,0 +1,30 @@
+<?php
+	
+?>
+<style>
+.user_login
+{
+	background: blue;
+	border: 1px solid navy;
+	width: 200px;
+	height: 200px;
+}
+</style>
+<script>
+
+function tryLogin() {
+	let user = document.getElementById('user');
+	let pass = document.getElementById('guacamole');
+	
+	// call try endpoint - returns status + callback id.
+	// the callback id will be for a temporary route that will establish our session.
+	console.log(user.value);
+	console.log(pass.value);
+}
+
+</script>
+<div class="user_login">
+	<input type="text" name="user" placeholder="Username/Email" id="user" />
+	<input type="password" name="guacamole" id="guacamole" />
+	<input type="button" name="login" id="login" onclick="tryLogin()"/>
+</div>
